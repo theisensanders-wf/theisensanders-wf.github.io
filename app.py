28082283f131294aea2132355384f8dc2134ac1a
Theisen Sanders <theisen.sanders@gmail.com>
2013-03-19 17:36:33 -0500

    Under construction template

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('base.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
