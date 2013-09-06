6894e940d8a7cd85635416c9312aecdeca4eb9f9
Theisen Sanders <theisen.sanders@gmail.com>
2013-09-05 23:22:18 -0500

    Cleanup old code

--- app.py
+++ app.py
@@ -1,29 +1,18 @@
 import os
 from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
 
 import settings
 
 app = Flask(__name__)
-app.debug = True
 
 app.config.from_object(settings)
-db = SQLAlchemy(app)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/admin')
-def admin():
-    return render_template('admin/base.html')
-
-@app.route('/login')
-def admin():
-    return render_template('admin/login.html')
-
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
--- settings.py
+++ settings.py
@@ -1,10 +1,1 @@
-from os import environ
-
-SECRET_KEY = 'this-is-the-secret-key-for-theisen-sanders'
-DROPBOX_KEY = 'bd0brhufrmn92s5'
-DROPBOX_SECRET = 'i6dv9ioihm4b69t'
-DROPBOX_ACCESS_TYPE = 'app_folder'
-
-SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
-if not SQLALCHEMY_DATABASE_URI:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://theisen:sanders@localhost/database'
+SECRET_KEY = 'mercury-venus-earth-mars-jupiter-saturn-uranus-neptune-pluto'
